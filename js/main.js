38e8b8f229027a0ad2336597293d561f6a86e40b
Ryan Purdy <Ryanpurdy@Ryans-MacBook-Pro-2.local>
2016-04-29 09:00:45 -0400

    test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,144 @@
-var $placeholder = $('.placeholder');
+var $LongTermMemory = $('.longtermmemory');
+var $Counting = $('.counting');
+var $Awareness = $('.awareness');
+var $Culture = $('.culture');
+var $Emotions = $('.emotions');
+var $Communication = $('.communication');
+var $Humor = $('.humor');
+var $Construction = $('.construction');
+var $Speed = $('.speed');
+var $Eyesight = $('.eyesight');
+var $Hearing = $('.hearing');
+var $Lifespan = $('.lifespan');
+var $Anisize = $('.anisize');
+var $Distance = $('.distance');
+var $Biodiversity = $('.biodiversity');
+var $Graph = $('.graph');
+
+$LongTermMemory.waypoint(function (direction) {
+  if (direction == 'down') {
+    $LongTermMemory.addClass('opacity')
+  } else {
+    $LongTermMemory.removeClass('opacity')
+  }
+}, { offset: '90%'} );
+
+$Counting.waypoint(function (direction) {
+  if (direction == 'down') {
+    $Counting.addClass('opacity')
+  } else {
+    $Counting.removeClass('opacity')
+  }
+}, { offset: '90%'} );
+
+$Awareness.waypoint(function (direction) {
+  if (direction == 'down') {
+    $Awareness.addClass('opacity')
+  } else {
+    $Awareness.removeClass('opacity')
+  }
+}, { offset: '90%'} );
+
+$Culture.waypoint(function (direction) {
+  if (direction == 'down') {
+    $Culture.addClass('opacity')
+  } else {
+    $Culture.removeClass('opacity')
+  }
+}, { offset: '90%'} );
+
+$Emotions.waypoint(function (direction) {
+  if (direction == 'down') {
+    $Emotions.addClass('opacity')
+  } else {
+    $Emotions.removeClass('opacity')
+  }
+}, { offset: '90%'} );
+
+$Communication.waypoint(function (direction) {
+  if (direction == 'down') {
+    $Communication.addClass('opacity')
+  } else {
+    $Communication.removeClass('opacity')
+  }
+}, { offset: '90%'} );
+
+$Humor.waypoint(function (direction) {
+  if (direction == 'down') {
+    $Humor.addClass('opacity')
+  } else {
+    $Humor.removeClass('opacity')
+  }
+}, { offset: '90%'} );
+
+$Construction.waypoint(function (direction) {
+  if (direction == 'down') {
+    $Construction.addClass('opacity')
+  } else {
+    $Construction.removeClass('opacity')
+  }
+}, { offset: '90%'} );
+
+$Speed.waypoint(function (direction) {
+  if (direction == 'down') {
+    $Speed.addClass('opacity')
+  } else {
+    $Speed.removeClass('opacity')
+  }
+}, { offset: '90%'} );
+
+$Eyesight.waypoint(function (direction) {
+  if (direction == 'down') {
+    $Eyesight.addClass('opacity')
+  } else {
+    $Eyesight.removeClass('opacity')
+  }
+}, { offset: '90%'} );
+
+$Hearing.waypoint(function (direction) {
+  if (direction == 'down') {
+    $Hearing.addClass('opacity')
+  } else {
+    $Hearing.removeClass('opacity')
+  }
+}, { offset: '90%'} );
+
+$Lifespan.waypoint(function (direction) {
+  if (direction == 'down') {
+    $Lifespan.addClass('opacity')
+  } else {
+    $Lifespan.removeClass('opacity')
+  }
+}, { offset: '90%'} );
+
+$Anisize.waypoint(function (direction) {
+  if (direction == 'down') {
+    $Anisize.addClass('opacity')
+  } else {
+    $Anisize.removeClass('opacity')
+  }
+}, { offset: '90%'} );
+
+$Distance.waypoint(function (direction) {
+  if (direction == 'down') {
+    $Distance.addClass('opacity')
+  } else {
+    $Distance.removeClass('opacity')
+  }
+}, { offset: '90%'} );
+
+$Biodiversity.waypoint(function (direction) {
+  if (direction == 'down') {
+    $Biodiversity.addClass('opacity')
+  } else {
+    $Biodiversity.removeClass('opacity')
+  }
+}, { offset: '90%'} );
+
+$Graph.waypoint(function (direction) {
+  if (direction == 'down') {
+    $Graph.addClass('opacity')
+  } else {
+    $Graph.removeClass('opacity')
+  }
+}, { offset: '90%'} );
